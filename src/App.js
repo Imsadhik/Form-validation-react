5dc3064d598c7925efa34443f2dae98e62a19192
sadhik <sadhikshaik507@gmail.com>
2023-06-27 23:14:04 +0530

    Form Validation

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         {/* <input type="text" placeholder='Enter user name'></input>
+         <br /> <br />
+         <input type="password" placeholder='Enter Password'></input>
+         <br /> <br/> */}
+        <h1>Hello world</h1>
     </div>
   );
 }
